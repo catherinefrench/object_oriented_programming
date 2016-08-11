89102edaeed13a801ecd08c648f7c3fe29317885
Catherine French <catherine.french@me.com>
2016-08-11 14:57:50 -0400

    Created two instances of the Cat class.

--- cat.rb
+++ cat.rb
@@ -1,5 +1,13 @@
-class Cat
+class Cat #creates class called Cat
 
-  def initialize(name, preferred_food, meal_time)
+  def initialize(name, preferred_food, meal_time) #initializes three attributes
+    @preferred_foods = preferred_food #sets instance variables
+    @meal_time = meal_time
+    @name = name
   end
+
+
 end
+
+cat1 = Cat.new("Whiskers", "salmon", 20) #Creates an instance of the Cat class
+cat2 = Cat.new("Ludwig", "chicken", 4)
